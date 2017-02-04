e7efd6e9ffad87d0fee9b7173193f2031cb13848
Hunter Stanton <hunter_stanton@outlook.com>
2017-02-04 01:57:33 -0500

    Initial Commit

---
+++ Song Title Generator WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Song Title Generator WatchKit Extension
+//
+//  Created by Hunter Stanton on 2/4/17.
+//  Copyright © 2017 Hunter Stanton. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Song Title Generator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Song Title Generator
+//
+//  Created by Hunter Stanton on 2/4/17.
+//  Copyright © 2017 Hunter Stanton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
